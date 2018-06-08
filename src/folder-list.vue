5f884f5658575104143bc7a7187d874c38ac52c5
Joshua J. Berry <des@condordes.net>
2018-06-08 11:09:18 -0700

    Drag to reorder tab groups

--- src/folder-list.vue
+++ src/folder-list.vue
@@ -1,19 +1,33 @@
 <template>
 <div>
-  <div v-for="f in folders" :key="f.title">
-    <folder v-bind="f"></folder>
-  </div>
+  <draggable v-model="folders" @sort="move">
+    <folder v-for="f in folders" :key="f.title" v-bind="f" :data-id="f.id">
+    </folder>
+  </draggable>
 </div>
 </template>
 
 <script>
+import Draggable from 'vuedraggable';
 import Folder from 'folder.vue';
+
 export default {
-    components: {Folder},
+    components: {Draggable, Folder},
     props: {folders: Array},
     data: () => ({}),
     computed: {},
-    methods: {},
+    methods: {
+        move: function(ev) {
+            //console.log('move tab', ev);
+            //console.log('item', ev.item.dataset.id);
+            //console.log('from', ev.oldIndex, ev.from.dataset.id);
+            //console.log('to', ev.newIndex, ev.to.dataset.id);
+
+            browser.bookmarks.move(ev.item.dataset.id, {
+                index: ev.newIndex,
+            }).then(() => {});
+        },
+    },
 };
 </script>
 
