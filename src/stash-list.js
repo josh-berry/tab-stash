02036c57761529d9913ac1ba9cd61f8a66487d57
Joshua J. Berry <des@condordes.net>
2018-07-01 15:19:18 -0700

    Load the sidebar a little faster

--- src/stash-list.js
+++ src/stash-list.js
@@ -12,9 +12,10 @@ import {StashState} from './model';
 
 
 window.addEventListener('load', asyncEvent(async function() {
-    let state = await StashState.make();
-    let root = await rootFolder();
-    let win = await browser.windows.getCurrent();
+    let state_p = StashState.make();
+    let root_p = rootFolder();
+    let win_p = browser.windows.getCurrent();
+    let [state, root, win] = [await state_p, await root_p, await win_p];
 
     const vue = new (Vue.extend(StashList))({data: {
         unstashed_tabs: state.wins_by_id.get(win.id).children,
