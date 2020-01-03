4d3d70093474ec810fc83e5e81d225066b8c1930
Joshua J. Berry <des@condordes.net>
2020-01-03 14:18:19 -0800

    Drop old screenshots and update my screenshot script

--- doc/resize-ff-devedition-for-screenshots.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1280, 800}'
---
+++ resize-ff-devedition-for-screenshots.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+#osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1280, 800}'
+
+# Reduce size for the artificial shadow added by the preview snapshots
+osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1168, 688}'
