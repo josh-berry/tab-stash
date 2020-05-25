d4c2a04d526cd496ae3cbae30ed85f64e0eec303
Joshua J. Berry <des@condordes.net>
2020-05-24 23:53:34 -0700

    Update resize script for new AMO dimensions

--- resize-ff-devedition-for-screenshots.sh
+++ resize-ff-devedition-for-screenshots.sh
@@ -3,4 +3,8 @@
 #osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1280, 800}'
 
 # Reduce size for the artificial shadow added by the preview snapshots
-osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1168, 688}'
+#osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1168, 688}'
+
+# HiDPI - based on new guidance from AMO, with subtractions to account for
+# window shadow
+osascript -e 'set the bounds of the first window of application "Firefox Developer Edition" to {0, 0, 1200-112, 900-112}'
