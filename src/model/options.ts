9304e316e7d2d1453c5f0b61fba3ee4042116999
Joshua J. Berry <des@condordes.net>
2020-10-18 18:28:12 -0700

    Add option to expire deleted items

--- src/model/options.ts
+++ src/model/options.ts
@@ -24,6 +24,9 @@ const SYNC_DEF = {
     // If the most recent unnamed folder is older than <X> minutes ago, we will
     // create a new folder instead of appending to the existing one.
     new_folder_timeout_min: {default: 5, is: aNumber},
+
+    // How long should we keep deleted items for?
+    deleted_items_expiration_days: {default: 180, is: aNumber},
 };
 
 const LOCAL_DEF = {
--- src/options.vue
+++ src/options.vue
@@ -1,45 +1,62 @@
 <template>
-<div>
-  <label for="meta_show_advanced"><div class="advanced">
-      <input type="checkbox" id="meta_show_advanced"
-             v-model="meta_show_advanced" />
-      Show advanced settings
-  </div></label>
+<main :class="{'show-advanced': meta_show_advanced}">
+  <ul>
+    <li class="show-advanced advanced">
+      <label for="meta_show_advanced">
+          <input type="checkbox" id="meta_show_advanced"
+                v-model="meta_show_advanced" />
+          Show advanced settings
+      </label>
+    </li>
+  </ul>
 
   <hr>
 
   <h4>Tab Stash Behavior (All Synced Browsers)</h4>
   <ul>
-    <li class="synced">When saving tabs to the stash:</li>
-    <ul>
-      <label for="open_stash_in_sidebar"><li>
-          <input type="radio" name="open_stash_in" id="open_stash_in_sidebar"
-                 v-model="open_stash_in" value="sidebar" />
-          Automatically show your stashed tabs in the sidebar
-      </li></label>
-      <label for="open_stash_in_tab"><li>
-          <input type="radio" name="open_stash_in" id="open_stash_in_tab"
-                 v-model="open_stash_in" value="tab" />
-          Automatically show your stashed tabs in a new tab
-      </li></label>
-      <label for="open_stash_in_none"><li>
-          <input type="radio" name="open_stash_in" id="open_stash_in_none"
-                 v-model="open_stash_in" value="none" />
-          Don't automatically show the stash
-      </li></label>
-    </ul>
-
-    <li v-if="meta_show_advanced" class="advanced">
-      <div>When stashing a single tab:</div>
+    <li class="synced">
+      <label>When saving tabs to the stash:</label>
       <ul>
-        <li title="If the top-most stash has a name, or is older than this, a new, unnamed stash will be created instead.">
-          <label for="new_folder_timeout_min">
-            Append to the top-most stash only if it was created in the last
-            <input type="number" id="new_folder_timeout_min"
-                   v-model="new_folder_timeout_min" min="0">
-            minutes
-          </label>
-        </li>
+        <li><label for="open_stash_in_sidebar">
+            <input type="radio" name="open_stash_in" id="open_stash_in_sidebar"
+                  v-model="open_stash_in" value="sidebar" />
+            Automatically show your stashed tabs in the sidebar
+        </label></li>
+        <li><label for="open_stash_in_tab">
+            <input type="radio" name="open_stash_in" id="open_stash_in_tab"
+                  v-model="open_stash_in" value="tab" />
+            Automatically show your stashed tabs in a new tab
+        </label></li>
+        <li><label for="open_stash_in_none">
+            <input type="radio" name="open_stash_in" id="open_stash_in_none"
+                  v-model="open_stash_in" value="none" />
+            Don't automatically show the stash
+        </label></li>
+      </ul>
+    </li>
+
+    <li>
+      <label>When deleting items from your stash:</label>
+      <ul>
+        <li><label for="deleted_items_expiration_days">
+          Remember items that were deleted on this computer for
+          <input type="number" id="deleted_items_expiration_days"
+                  v-model="deleted_items_expiration_days" min="1">
+          days
+        </label></li>
+      </ul>
+    </li>
+
+    <li class="advanced">
+      <label>When stashing a single tab:</label>
+      <ul>
+        <li><label for="new_folder_timeout_min"
+                   title="If the top-most stash has a name, or is older than this, a new, unnamed stash will be created instead.">
+          Append to the top-most stash only if it was created in the last
+          <input type="number" id="new_folder_timeout_min"
+                  v-model="new_folder_timeout_min" min="0">
+          minutes
+        </label></li>
       </ul>
     </li>
   </ul>
@@ -48,84 +65,85 @@
 
   <h4 title="Options in this section are stored only on this computer.">Tab and Memory Management (This Browser)</h4>
   <ul>
-    <li>Once a tab has been stashed:</li>
-    <ul>
-      <label for="after_stashing_tab_hide">
-        <li title="Hidden tabs that are still loaded can be restored instantly. They also preserve anything you had entered into the tab and its history (e.g. Back button).">
-          <input type="radio"
-                 name="after_stashing_tab" id="after_stashing_tab_hide"
-                 v-model="after_stashing_tab" value="hide" />
-          Hide the tab and keep it loaded in the background
+    <li>
+      <label>Once a tab has been stashed:</label>
+      <ul>
+        <li>
+          <label for="after_stashing_tab_hide"
+                 title="Hidden tabs that are still loaded can be restored instantly. They also preserve anything you had entered into the tab and its history (e.g. Back button).">
+            <input type="radio"
+                  name="after_stashing_tab" id="after_stashing_tab_hide"
+                  v-model="after_stashing_tab" value="hide" />
+            Hide the tab and keep it loaded in the background
+          </label>
+          <ul :class="{disabled: after_stashing_tab !== 'hide'}">
+            <li>
+              <label for="autodiscard_hidden_tabs"
+                    title="Monitors the total number of tabs in your browser, and if you have a lot of tabs open, unloads hidden tabs that haven't been used recently. This option significantly reduces memory usage and is recommended for most users.">
+                <input type="checkbox"
+                      name="autodiscard_hidden_tabs" id="autodiscard_hidden_tabs"
+                      :disabled="after_stashing_tab !== 'hide'"
+                      v-model="autodiscard_hidden_tabs" />
+                Automatically unload hidden tabs that haven't been used in a while
+              </label>
+              <ul :class="{advanced: true,
+                           disabled: after_stashing_tab !== 'hide'
+                                     || ! autodiscard_hidden_tabs}">
+                <li><label for="autodiscard_interval_min">
+                    Check for old hidden tabs every
+                    <input type="number" id="autodiscard_interval_min"
+                          :disabled="after_stashing_tab !== 'hide'
+                                      || ! autodiscard_hidden_tabs"
+                          v-model="autodiscard_interval_min" min="1">
+                    minutes
+                </label></li>
+                <li><label for="autodiscard_min_keep_tabs">
+                    Keep at least
+                    <input type="number" id="autodiscard_min_keep_tabs"
+                          :disabled="after_stashing_tab !== 'hide'
+                                      || ! autodiscard_hidden_tabs"
+                          v-model="autodiscard_min_keep_tabs" min="0">
+                    hidden and visible tabs loaded at all times
+                </label></li>
+                <li><label for="autodiscard_target_age_min">
+                    Keep the oldest tabs loaded for at least
+                    <input type="number" id="autodiscard_target_age_min"
+                          :disabled="after_stashing_tab !== 'hide'
+                                      || ! autodiscard_hidden_tabs"
+                          v-model="autodiscard_target_age_min" min="1">
+                    minutes, but...
+                </label></li>
+                <li><label for="autodiscard_target_tab_count">
+                    ...unload tabs more aggressively if there are more than
+                    <input type="number" id="autodiscard_target_tab_count"
+                          :disabled="after_stashing_tab !== 'hide'
+                                      || ! autodiscard_hidden_tabs"
+                          v-model="autodiscard_target_tab_count"
+                          :min="autodiscard_min_keep_tabs">
+                    tabs loaded
+                </label></li>
+              </ul>
+            </li>
+          </ul>
         </li>
-      </label>
-      <ul :class="{disabled: after_stashing_tab !== 'hide'}">
-        <label for="autodiscard_hidden_tabs">
-          <li title="Monitors the total number of tabs in your browser, and if you have a lot of tabs open, unloads hidden tabs that haven't been used recently. This option significantly reduces memory usage and is recommended for most users.">
-            <input type="checkbox"
-                   name="autodiscard_hidden_tabs" id="autodiscard_hidden_tabs"
-                   :disabled="after_stashing_tab !== 'hide'"
-                   v-model="autodiscard_hidden_tabs" />
-            Automatically unload hidden tabs that haven't been used in a while
-          </li>
-        </label>
-        <ul v-if="meta_show_advanced"
-            :class="{advanced: true,
-                    disabled: after_stashing_tab !== 'hide'
-                           || ! autodiscard_hidden_tabs}">
-          <label for="autodiscard_interval_min"><li>
-              Check for old hidden tabs every
-              <input type="number" id="autodiscard_interval_min"
-                     :disabled="after_stashing_tab !== 'hide'
-                                || ! autodiscard_hidden_tabs"
-                     v-model="autodiscard_interval_min" min="1">
-              minutes
-          </li></label>
-          <label for="autodiscard_min_keep_tabs"><li>
-              Keep at least
-              <input type="number" id="autodiscard_min_keep_tabs"
-                     :disabled="after_stashing_tab !== 'hide'
-                                || ! autodiscard_hidden_tabs"
-                     v-model="autodiscard_min_keep_tabs" min="0">
-              hidden and visible tabs loaded at all times
-          </li></label>
-          <label for="autodiscard_target_age_min"><li>
-              Keep the oldest tabs loaded for at least
-              <input type="number" id="autodiscard_target_age_min"
-                     :disabled="after_stashing_tab !== 'hide'
-                                || ! autodiscard_hidden_tabs"
-                     v-model="autodiscard_target_age_min" min="1">
-              minutes, but...
-          </li></label>
-          <label for="autodiscard_target_tab_count"><li>
-              ...unload tabs more aggressively if there are more than
-              <input type="number" id="autodiscard_target_tab_count"
-                     :disabled="after_stashing_tab !== 'hide'
-                                || ! autodiscard_hidden_tabs"
-                     v-model="autodiscard_target_tab_count"
-                     :min="autodiscard_min_keep_tabs">
-              tabs loaded
-          </li></label>
-        </ul>
-      </ul>
-      <label for="after_stashing_tab_hide_discard">
-        <li title="Hidden tabs that are unloaded can be restored very quickly, and usually without a network connection. They also preserve browsing history (e.g. Back button). However, depending on the website, you may lose anything you had entered into the tab that isn't already saved. Uses less memory.">
+        <li><label for="after_stashing_tab_hide_discard"
+                   title="Hidden tabs that are unloaded can be restored very quickly, and usually without a network connection. They also preserve browsing history (e.g. Back button). However, depending on the website, you may lose anything you had entered into the tab that isn't already saved. Uses less memory.">
           <input type="radio"
-                 name="after_stashing_tab" id="after_stashing_tab_hide_discard"
-                 v-model="after_stashing_tab" value="hide_discard" />
+                name="after_stashing_tab" id="after_stashing_tab_hide_discard"
+                v-model="after_stashing_tab" value="hide_discard" />
           Hide the tab and immediately unload it
-        </li>
-      </label>
-      <label for="after_stashing_tab_close">
-        <li title="Uses the least amount of memory and takes the longest to restore, because tab contents may need to be fetched over the network. This option will cause the browser to forget anything you had entered into the tab, as well as the tab's browsing history (e.g. Back button).">
+        </label></li>
+        <li><label for="after_stashing_tab_close"
+                   title="Uses the least amount of memory and takes the longest to restore, because tab contents may need to be fetched over the network. This option will cause the browser to forget anything you had entered into the tab, as well as the tab's browsing history (e.g. Back button).">
           <input type="radio"
-                 name="after_stashing_tab" id="after_stashing_tab_close"
-                 v-model="after_stashing_tab" value="close" />
+                name="after_stashing_tab" id="after_stashing_tab_close"
+                v-model="after_stashing_tab" value="close" />
           Close the tab immediately
-        </li>
-      </label>
-    </ul>
+        </label></li>
+      </ul>
+    </li>
   </ul>
-</div>
+</main>
 </template>
 
 <script lang="ts">
