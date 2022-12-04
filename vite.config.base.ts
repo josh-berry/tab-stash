a54df0f4c5a4690002abc7812f5e2c0dabb45086
Joshua J. Berry <des@condordes.net>
2022-12-04 14:51:01 -0800

    Tweak Vite options for reproducible builds

--- vite.config.base.ts
+++ vite.config.base.ts
@@ -6,6 +6,8 @@ import {type UserConfig} from "vite";
 
 const rel = (p: string) => resolve(__dirname, p);
 
+const prod = process.env.NODE_ENV === "production";
+
 // https://vitejs.dev/config/
 export default {
   root: "src",
@@ -25,5 +27,18 @@ export default {
     outDir: rel("dist"),
     sourcemap: true,
     emptyOutDir: false,
+    minify: prod ? "esbuild" : false,
+
+    // Remove the hash from the generated file names, because it (and ONLY it;
+    // the content is the same even though it's supposedly a "content hash")
+    // seems to be inconsistent from build to build depending on the path to the
+    // build tree.
+    rollupOptions: {
+      output: {
+        assetFileNames: "assets/[name].[ext]",
+        chunkFileNames: "assets/[name].js",
+        entryFileNames: "[name].js",
+      },
+    },
   },
 } as UserConfig;
--- vite.config.html.ts
+++ vite.config.html.ts
@@ -1,13 +1,11 @@
 import base from "./vite.config.base";
 
-base.build!.rollupOptions = {
-  input: {
-    "deleted-items": "src/deleted-items.html",
-    restore: "src/restore.html",
-    "stash-list": "src/stash-list.html",
-    options: "src/options.html",
-    "whats-new": "src/whats-new.html",
-  },
+base.build!.rollupOptions!.input = {
+  "deleted-items": "src/deleted-items.html",
+  restore: "src/restore.html",
+  "stash-list": "src/stash-list.html",
+  options: "src/options.html",
+  "whats-new": "src/whats-new.html",
 };
 
 export default base;
