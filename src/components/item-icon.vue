be98bede350e1cc691783d4d1ebc2fe767e8388a
Joshua J. Berry <des@condordes.net>
2023-08-06 15:38:05 -0700

    item-icon: Drastically improve rendering performance in popup view

--- src/components/item-icon.vue
+++ src/components/item-icon.vue
@@ -1,3 +1,25 @@
+<!--
+  PERFORMANCE:
+  The <img> element below shows up a LOT -- once for every tab in the UI, so the
+  performance-tuning options below are helpful for page-load performance.
+
+  One critical thing that is intentionally omitted here is `srcset`. We used to
+  specify a srcset hint to encourage HiDPI scaling of the image, but this is no
+  longer necessary on recent Firefoxen, and it causes a major performance
+  regression in the popup view--instead of making Firefox decide which image to
+  use, we only give it a single `src`.
+
+  What happens is: Firefox tries to recalculate the size of the popup on every
+  DOM mutation(!)[1]. This is NOT something that is done in the tab or sidebar,
+  so represents additional overhead to DOM mutations in the popup.
+  Unfortunately, this can get quite expensive, because as part of this, Firefox
+  may reflow the _entire page_. Profiling showed that most of the time spent in
+  reflow was parsing the URLs on <img>'s attributes, and _that_ was happening
+  because Firefox was trying to decide which version of the image to use.
+  Avoiding this check makes reflow much, much faster.
+
+  [1]: https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/user_interface/Popups
+-->
 <template>
   <component
     :is="selectable ? 'a' : 'span'"
@@ -7,9 +29,10 @@
     <img
       v-if="src"
       :src="src"
-      :srcset="src + ' 2x'"
       referrerpolicy="no-referrer"
       alt=""
+      decoding="async"
+      loading="lazy"
     />
     <span v-else :class="{icon: true, [`icon-${defaultIcon}`]: true}" />
   </component>
