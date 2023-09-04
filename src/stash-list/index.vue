f6f32d6d5f439afb30f81f9a6de20af51bef2134
Joshua J. Berry <des@condordes.net>
2023-09-04 14:57:35 -0700

    Update new-version notification for 3.0

--- src/stash-list/index.vue
+++ src/stash-list/index.vue
@@ -18,8 +18,8 @@
         @activate="go('whats-new.html')"
         @dismiss="hideWhatsNew"
       >
-        Tab Stash {{ my_version }} includes small quality-of-life improvements.
-        See what's new!
+        Tab Stash {{ my_version }} adds sub-groups and a popup view, and sports
+        a refreshed look and feel. See what's new!
       </Notification>
       <Notification
         key="new-fixes"
@@ -27,8 +27,8 @@
         @activate="go('whats-new.html')"
         @dismiss="hideWhatsNew"
       >
-        Tab Stash {{ my_version }} includes small quality-of-life improvements.
-        See what's new!
+        Tab Stash {{ my_version }} adds sub-groups and a popup view, and sports
+        a refreshed look and feel. See what's new!
       </Notification>
       <Notification
         key="stash-root-warning"
