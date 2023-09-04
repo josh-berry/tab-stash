9f3703a5fb45c031df54fc800518695843fd3202
Joshua J. Berry <des@condordes.net>
2023-09-04 15:05:49 -0700

    Update install-deps.sh per Nodesource instructions
    https://github.com/nodesource/distributions#installation-instructions

--- install-deps.sh
+++ install-deps.sh
@@ -2,6 +2,8 @@
 
 set -ex
 
+NODE_VERSION=20
+
 deps_apt() {
     # Check for old Ubuntus
     if [ "$(lsb_release -i |awk '{print $3}')" = "Ubuntu" ]; then
@@ -32,10 +34,15 @@ deps_apt() {
     fi
 
     # Build tools
-    sudo apt-get install -y make git diffutils patch rsync zip
+    sudo apt-get install -y make git diffutils patch rsync zip ca-certificates curl gnupg
 
     if ! type node; then
-        curl -fsSL https://deb.nodesource.com/setup_current.x | sudo -E bash -
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
+
+        echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_VERSION.x nodistro main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+
+        sudo apt-get update
         sudo apt-get install -y nodejs
     fi
 }
